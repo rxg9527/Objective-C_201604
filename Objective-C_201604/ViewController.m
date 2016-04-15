b4c8fd2e9e493c1f1e4232abe1954bbd5a10c632
Yuen-rMBP <rxg9527@sina.cn>
2016-04-16 00:38:21 +0800

    0416 利用dispatch_group_t判断多个网络请求都结束 dispatch_group_enter(dispatch_group_t group) dispatch_group_leave(dispatch_group_t group) dispatch_group_notify(dispatch_group_t group, dispatch_queue_t queue, <#^(void)block#>)

--- Objective-C_201604/ViewController.m
+++ Objective-C_201604/ViewController.m
@@ -19,6 +19,10 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSError *requestURL1error;
+@property (nonatomic, strong) NSError *requestURL2error;
+@property (nonatomic, strong) NSMutableArray *successResults;
+
 @end
 
 @implementation ViewController
@@ -30,13 +34,23 @@
     [self downloadData];
 }
 
+- (NSMutableArray *)successResults {
+    if (!_successResults) {
+        _successResults = @[].mutableCopy;
+    }
+    return _successResults;
+}
+
 // 用AFNetWorking实现get请求
 - (void)downloadData
 {
     AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] init];
-    
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
+    dispatch_group_t serviceGroup = dispatch_group_create();
+    
+    // Start the first service
+    dispatch_group_enter(serviceGroup);
     [manager GET:requestURL1 parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         id result = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
         if ([result isKindOfClass:[NSDictionary class]]) {
@@ -44,11 +58,16 @@
             XGBaseClass *model = [XGBaseClass modelObjectWithDictionary:dict];
             NSLog(@"%@", [model dictionaryRepresentation]);
         }
-        
+        [self.successResults addObject:@(1)];
+        dispatch_group_leave(serviceGroup);
+
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"error:%@", error);
+        self.requestURL1error = error;
+        dispatch_group_leave(serviceGroup);
     }];
     
+    // Start the second service
+    dispatch_group_enter(serviceGroup);
     [manager GET:requestURL2 parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
         id result = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
         if ([result isKindOfClass:[NSDictionary class]]) {
@@ -56,9 +75,25 @@
             XGBaseClass *model = [XGBaseClass modelObjectWithDictionary:dict];
             NSLog(@"%@", [model dictionaryRepresentation]);
         }
+        [self.successResults addObject:@(1)];
+        dispatch_group_leave(serviceGroup);
     } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
-        NSLog(@"error:%@", error);
+        self.requestURL2error = error;
+        dispatch_group_leave(serviceGroup);
     }];
+    
+    dispatch_group_notify(serviceGroup, dispatch_get_main_queue(), ^{
+        // Assess any errors
+        NSError *overallError = nil;
+        if (self.requestURL1error || self.requestURL2error)
+        {
+            // Either make a new error or assign one of them to the overall error
+            overallError = self.requestURL1error ?: self.requestURL2error;
+        }
+        // Now call the final completion block
+        NSLog(@"%@", overallError);
+        NSLog(@"请求成功数：%zd", self.successResults.count);
+    });
 }
 
 @end
