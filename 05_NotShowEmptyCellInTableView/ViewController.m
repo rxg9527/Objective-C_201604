895914de405b56e9ba27bd24606d79d3eedcfa38
Yuen-rMBP <rxg9527@sina.cn>
2016-04-16 23:46:11 +0800

    0416 初始化

---
+++ 05_NotShowEmptyCellInTableView/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  05_NotShowEmptyCellInTableView
+//
+//  Created by Yuen on 16/4/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 05_NotShowEmptyCellInTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  05_NotShowEmptyCellInTableView
+//
+//  Created by Yuen on 16/4/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
