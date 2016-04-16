6db0dda085e8340b5603da2f8d962255fa282db1
Yuen-rMBP <rxg9527@sina.cn>
2016-04-16 23:38:15 +0800

    0416 初始化

---
+++ 04_static_inline/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  04_static_inline
+//
+//  Created by Yuen on 16/4/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
