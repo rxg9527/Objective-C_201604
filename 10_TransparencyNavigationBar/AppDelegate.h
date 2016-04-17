a103ac8a97faeab8e3449d8311506c2b5317c550
Yuen-rMBP <rxg9527@sina.cn>
2016-04-17 23:58:02 +0800

    0417 初始化

---
+++ 10_TransparencyNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10_TransparencyNavigationBar
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ 10_TransparencyNavigationBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  10_TransparencyNavigationBar
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
