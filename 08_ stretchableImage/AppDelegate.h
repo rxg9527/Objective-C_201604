9ae83961d0b3d67a67bc0d8358ca0086e32f9d57
Yuen-rMBP <rxg9527@sina.cn>
2016-04-17 23:26:13 +0800

    0417 初始化

---
+++ 08_ stretchableImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  08_ stretchableImage
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ 08_ stretchableImage/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  08_ stretchableImage
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *defaultImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *stretchableImageView;
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
