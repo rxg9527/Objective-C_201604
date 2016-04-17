e453b0bae9f2f844a66da4a8be9a30d72dee1e8f
Yuen-rMBP <rxg9527@sina.cn>
2016-04-17 22:36:52 +0800

    0417 初始化

---
+++ 07_panUISwipeGesture/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  07_panUISwipeGesture
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
---
+++ 07_panUISwipeGesture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  07_panUISwipeGesture
+//
+//  Created by Yuen on 16/4/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
