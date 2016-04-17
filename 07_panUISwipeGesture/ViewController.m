012a8914bc309bbdac943c523f29892016662fea
Yuen-rMBP <rxg9527@sina.cn>
2016-04-17 23:06:57 +0800

    0417 只用一个pan手势来代替UISwipeGesture的各个方向

--- 07_panUISwipeGesture/ViewController.m
+++ 07_panUISwipeGesture/ViewController.m
@@ -22,4 +22,93 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)pan:(UIPanGestureRecognizer *)sender
+{
+    typedef NS_ENUM(NSUInteger, UIPanGestureRecognizerDirection) {
+        UIPanGestureRecognizerDirectionUndefined,
+        UIPanGestureRecognizerDirectionUp,
+        UIPanGestureRecognizerDirectionDown,
+        UIPanGestureRecognizerDirectionLeft,
+        UIPanGestureRecognizerDirectionRight
+    };
+
+    static UIPanGestureRecognizerDirection direction = UIPanGestureRecognizerDirectionUndefined;
+
+    switch (sender.state) {
+        case UIGestureRecognizerStateBegan: {
+            if (direction == UIPanGestureRecognizerDirectionUndefined) {
+                CGPoint velocity = [sender velocityInView:sender.view];
+
+                /**
+                 *  是否主要是竖直方向上的手势
+                 *  fabs 对double型数据取绝对值
+                 */
+                BOOL isVerticalGesture = fabs(velocity.y) > fabs(velocity.x);
+
+                if (isVerticalGesture) {
+                    if (velocity.y > 0) {
+                        direction = UIPanGestureRecognizerDirectionDown;
+                    } else {
+                        direction = UIPanGestureRecognizerDirectionUp;
+                    }
+                } else {
+                    if (velocity.x > 0) {
+                        direction = UIPanGestureRecognizerDirectionRight;
+                    } else {
+                        direction = UIPanGestureRecognizerDirectionLeft;
+                    }
+                }
+            }
+            break;
+        }
+
+        case UIGestureRecognizerStateChanged: {
+            switch (direction) {
+                case UIPanGestureRecognizerDirectionUp: {
+                    [self handleUpwardsGesture:sender];
+                    break;
+                }
+                case UIPanGestureRecognizerDirectionDown: {
+                    [self handleDownwardsGesture:sender];
+                    break;
+                }
+                case UIPanGestureRecognizerDirectionLeft: {
+                    [self handleLeftGesture:sender];
+                    break;
+                }
+                case UIPanGestureRecognizerDirectionRight: {
+                    [self handleRightGesture:sender];
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+
+        case UIGestureRecognizerStateEnded: {
+            direction = UIPanGestureRecognizerDirectionUndefined;
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+- (void)handleRightGesture:(UIPanGestureRecognizer *)recognizer
+{
+}
+
+- (void)handleLeftGesture:(UIPanGestureRecognizer *)recognizer
+{
+}
+
+- (void)handleDownwardsGesture:(UIPanGestureRecognizer *)recognizer
+{
+}
+
+- (void)handleUpwardsGesture:(UIPanGestureRecognizer *)recognizer
+{
+}
+
+
 @end
