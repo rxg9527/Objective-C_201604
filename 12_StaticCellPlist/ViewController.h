2bb85679037b9911170f128452cd3e1ed1bcd32f
Yuen-rMBP <rxg9527@sina.cn>
2016-04-19 23:03:52 +0800

    0419 初始化，添加TableView

---
+++ 12_StaticCellPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  12_StaticCellPlist
+//
+//  Created by Yuen on 16/4/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ 12_StaticCellPlist/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  12_StaticCellPlist
+//
+//  Created by Yuen on 16/4/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
