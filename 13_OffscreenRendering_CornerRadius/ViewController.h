662b10f07f5d0d041d4c585c8c287f4336c0cf6c
Yuen-rMBP <rxg9527@sina.cn>
2016-04-20 22:41:15 +0800

    初始化

---
+++ 13_OffscreenRendering_CornerRadius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  13_OffscreenRendering_CornerRadius
+//
+//  Created by Yuen on 16/4/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ 13_OffscreenRendering_CornerRadius/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  13_OffscreenRendering_CornerRadius
+//
+//  Created by Yuen on 16/4/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
