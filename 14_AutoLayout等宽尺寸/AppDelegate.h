87d97b96d541c759d227717c65f56234c88fd96a
Yuen-rMBP <rxg9527@sina.cn>
2016-04-27 23:08:45 +0800

    0427 AutoLayout等宽尺寸

---
+++ 14_AutoLayout等宽尺寸/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14_AutoLayout等宽尺寸
+//
+//  Created by Yuen on 16/4/27.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 14_AutoLayout等宽尺寸/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  14_AutoLayout等宽尺寸
+//
+//  Created by Yuen on 16/4/27.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
